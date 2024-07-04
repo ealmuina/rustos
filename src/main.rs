acc3b730353bbc0542b03cf73d51a0053ac0885a
Eddy Almuiña <almuina.e@gmail.com>
2024-07-04 21:07:20 +0200

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rustos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
+
 
 /// This function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+/// Panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rustos::test_panic_handler(info)
+}
+
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("aaaaa");
+
+    #[cfg(test)]
+    //noinspection RsUnresolvedPath
+    {
+        test_main();
+    }
 
     loop {}
 }
